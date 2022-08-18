75cb1591cef9034641c48564bb914d1902cd7a0c
workmatt <matthew@zetaton.com>
2022-08-18 11:17:44 -0500

    refactor regiter to match java api

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:7886/users/";
+const API_URL = "http://localhost:7886/";
 
 const register = (submit) => {
-    return axios.post(API_URL + "register", submit, {headers: ""})
+    return axios.post(API_URL + "authenticate/register", submit, {headers: ""})
         .then((response) => {
             return response.data;
         });
